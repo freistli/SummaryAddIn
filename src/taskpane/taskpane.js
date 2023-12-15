8183fc1b2edc4732bd6027da12f5261c166a00e3
Freist Li <fauna179@hotmail.com>
2023-12-15 17:18:05 +0800

    support different lanugage summarization, and add  configuration for endpoint

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -11,14 +11,19 @@ Office.onReady((info) => {
     document.getElementById("app-body").style.display = "flex";
     document.getElementById("run").onclick = run;
     document.getElementById("summary").onclick = summary;
+
+    document.getElementById("username").innerHTML = "Welcome " + Office.context.mailbox.userProfile.displayName + " ("+ Office.context.mailbox.userProfile.emailAddress +") !";
+
   }
 });
+
+ 
 
 export async function run() {
   // Get a reference to the current message
   const item = Office.context.mailbox.item;
 
-  // Write message property value to the task pane
+   // Write message property value to the task pane
   document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + item.subject;
 
   Office.context.mailbox.item.body.getAsync(
@@ -27,7 +32,7 @@ export async function run() {
     function callback(result) {
         if (result.status === "succeeded") {
             console.log("The body is " + result.value);
-            document.getElementById("item-content").innerHTML = result.value;
+            document.getElementById("item-content").value = result.value;
 
         } else {
             console.log("Failed to get body. Error: " + result.error);
@@ -36,8 +41,16 @@ export async function run() {
 );
   }
 export async function summary() {
-           const content = document.getElementById("item-content").innerHTML;
-           const url = "https://<LLMAPI>.azurewebsites.net/api/summarize";
+
+          var selectElement = document.getElementById('language');
+          var selectedValue = selectElement.value;
+          var config = require('../config/config.json');
+          console.log(selectedValue);
+           const content = document.getElementById("item-content").value;
+           // Record the start time
+          var startTime = Date.now();
+           document.getElementById("item-summarize").innerHTML = "Loading.......";
+           const url = config.endpoint +selectedValue;
            
             const response = await fetch(url, {
               method: "POST",
@@ -49,8 +62,15 @@ export async function summary() {
             .then((data) =>
             {
               console.log(data);
-              document.getElementById("item-summarize").innerHTML = data;
-            } );
+              // Record the end time
+              var endTime = Date.now();
+
+              // Calculate the time taken
+              var timeTaken = (endTime - startTime) / 1000;
+
+              document.getElementById("item-summarize").innerHTML = 'Loaded in ' + timeTaken + ' seconds. \r\n' + data;
+            } )
+            .catch((error) => document.getElementById("item-summarize").innerHTML= 'Loaded in ' + timeTaken + ' seconds. \r\n' + error );
           }
 
 
